176656cb16c352fdc942383d5ca141c8dfba897b
sara <yisethsara094@gmail.com>
2026-08-27 15:55:36 -0500

    automatizacion

---
+++ database/factories/RoleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
+ */
+class RoleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre_rol' => fake()->unique()->word(),
+            // Se eliminó 'descripcion' porque no está en tu migración de roles
+        ];
+    }
+}
